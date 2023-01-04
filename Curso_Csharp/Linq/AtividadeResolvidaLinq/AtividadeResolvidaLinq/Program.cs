ca4708e3167671951e4a10c9c9590847daa4905f
lucasmormilho <56029129+lucasmormilho@users.noreply.github.com>
2023-01-04 17:22:11 -0300

    Fim csharp basico

--- Curso_Csharp/Linq/AtividadeResolvidaLinq/AtividadeResolvidaLinq/Program.cs
+++ Curso_Csharp/Linq/AtividadeResolvidaLinq/AtividadeResolvidaLinq/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AtividadeResolvidaLinq.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
 
 namespace AtividadeResolvidaLinq
 {
@@ -6,7 +11,46 @@ namespace AtividadeResolvidaLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Aulas\Gitlucas\Curso_Csharp\linqArquivo.csv";
+            List<Produto> prod = new List<Produto>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] separar = sr.ReadLine().Split(",");
+                        string nome = separar[0];
+                        double preco = double.Parse(separar[1], CultureInfo.InvariantCulture);
+                        prod.Add(new Produto(nome, preco));
+                    }
+                }
+
+                foreach(Produto item in prod)
+                {
+                    Console.WriteLine(item.Nome + " - " + item.Preco);
+                }
+
+                Console.WriteLine();
+                var x = prod.Where(x => x.Preco >= 0.0).Average(a => a.Preco);
+                Console.WriteLine("Média total: "  + x.ToString("F2"));
+
+                Console.WriteLine();
+                Console.WriteLine("Produtos com preço abaixo da média:");
+                var y = prod.Where(a => a.Preco < x).OrderByDescending(x => x.Nome).Select(p => p.Nome);
+                foreach (string item in y)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("erro: " + e);
+            }
+
+            
+
         }
     }
 }
---
+++ Curso_Csharp/Linq/LinqExercicioFinal/LinqExercicioFinal/Program.cs
@@ -0,0 +1,59 @@
+﻿using LinqExercicioFinal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace LinqExercicioFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:\Aulas\Gitlucas\Curso_Csharp\Linq\linqExercicioFinal.csv
+
+            Console.Write("Endereço do arquivo: ");
+            string path = Console.ReadLine();
+
+            List<Funcionario> func = new List<Funcionario>();
+
+            Console.Write("Salario a validar: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Email das pessoas que recebem mais que o salario acima:");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] dados = sr.ReadLine().Split(",");
+                        double grana = double.Parse(dados[2], CultureInfo.InvariantCulture);
+                        Funcionario funcionario = new Funcionario(dados[0], dados[1], grana);
+                        func.Add(funcionario);
+                    }
+                }
+                var s = func
+                    .Where(x => x.Salario > salario).OrderBy(y => y.Email).Select(z => z.Email);
+
+                foreach(string item in s)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var soma = func.Where(x => x.Nome[0] == 'M').Sum(y => y.Salario);
+
+                Console.WriteLine("Soma dos salarios das pessoas que começam com a letra M: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("error: " + e);
+            }
+        }
+    }
+}
